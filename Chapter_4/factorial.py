ee16da3cb4e0522c7d3120edf2909bd1b7c6bfa3
Akashkewat5220 <102970062+Akashkewat5220@users.noreply.github.com>
2023-11-26 09:52:52 +0530

    Add files via upload

---
+++ Chapter_4/factorial.py
@@ -0,0 +1,14 @@
+#Factorial of a given number
+def factorial(n):
+    ans = 1
+    if n ==0:
+        ans = 1
+    else:
+        for i in range (1 , n+1):
+            ans *= i
+    return ans
+
+n = int(input("Enter n: "))
+
+output = factorial(n)
+print(output)
