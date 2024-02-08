425a30eb407aa6b0a609e978691ce023173998b0
Akashkewat5220 <102970062+Akashkewat5220@users.noreply.github.com>
2024-02-08 19:04:07 +0530

    Add files via upload

---
+++ Chapter_7 (OOPs)/ooops.py
@@ -0,0 +1,14 @@
+class students:
+
+    def set_name(self , name):
+        self.name = name
+
+        def get_name(self):
+            return self.name
+        
+    # Student1 = students()
+    # Student1.set_name("Arun")
+    # print(Student1.get_name())
+
+    # //NOT DONE COMPLETELY
+        #This was the last chapter i have done this with rohit bhaiya
